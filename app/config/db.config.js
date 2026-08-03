527aabdc6fff9c623f6003dce555a3fcff967b77
Daniel Puluc <jpulucr2@miumg.edu.gt>
2026-08-03 11:58:29 -0600

    agregar db.config.js

---
+++ app/config/db.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    HOST: "ep-bitter-waterfall-atwbsbys-pooler.c-9.us-east-1.aws.neon.tech",
+    USER: "neondb_owner",
+    PASSWORD: "npg_Ak7mGVIZ0HjC",
+    DB: "neondb",
+    dialect: "postgres",
+    pool: {
+        max: 5,
+        min: 0,
+        acquire: 30000,
+        idle: 10000
+    }
+};
+
+
